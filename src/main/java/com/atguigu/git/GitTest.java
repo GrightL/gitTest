38d50edf307b4604886be889726d0dac02742c96
GL <guoleijy@163.com>
2022-11-07 14:26:39 +0800

    commit3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("commit1");
         System.out.println("commit2");
+        System.out.println("commit3");
     }
 }
