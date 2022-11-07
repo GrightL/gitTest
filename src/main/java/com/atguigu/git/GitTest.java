95596858f482dd50adca241bd428643d8b251c69
GL <guoleijy@163.com>
2022-11-07 14:27:54 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("commit2");
         System.out.println("commit3");
         System.out.println("hot-fix commit1");
+        System.out.println("hot-fix commit2");
     }
 }
