c5bcbd465ff50fe5f6f32138c12dd8d3e8f48c46
GL <guoleijy@163.com>
2022-11-07 14:25:49 +0800

    commit1

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author GL
+ * @create 2022-11-07 14:20
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("commit1");
+    }
+}
